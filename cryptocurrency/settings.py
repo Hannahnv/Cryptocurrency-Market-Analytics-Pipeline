a08e21efbb0b52cdfb622988337ac85166f2968d
Hang Nguyen <102349995+Hannahnv@users.noreply.github.com>
2025-07-03 11:34:50 +0700

    settings.py

--- cryptocurrency/settings.py
+++ cryptocurrency/settings.py
@@ -76,13 +76,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cryptocurrency_db',
-        'USER': 'postgres',
-        'PASSWORD': 'DB_PASSWORD',  
+        'USER': 'your_username',
+        'PASSWORD': 'your_password',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
